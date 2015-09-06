52d918778da1e52fa97a0c4c73cfbfaea4b1fb97
Christoph Mathys <c.mathys@ucl.ac.uk>
2015-09-06 22:11:53 +0100

    Improved test_hgf.py

--- test_hgf.py
+++ test_hgf.py
@@ -1,4 +1,5 @@
 import pytest
+import pickle
 import numpy as np
 import hgf
 
@@ -16,8 +17,13 @@ def test_node_setup():
     x1.add_volatility_parent(parent=x2, kappa=1)
     xU.set_value_parent(parent=x1)
 
-    # Give an input
-    xU.input(0.5)
+    # Give some inputs
+    for u in [0.5, 0.5, 0.5]:
+        xU.input(u)
+
+    # Check if NegativePosteriorPrecisionError exception is correctly raised
+    with pytest.raises(hgf.NegativePosteriorPrecisionError):
+        xU.input(1e5)
 
     # Has update worked?
     assert x1.mus[1] == 1.0399909812322021
@@ -27,22 +33,26 @@ def test_node_setup():
 def test_standard_hgf():
     # Set up standard 2-level HGF for continuous inputs
     stdhgf = hgf.StandardHGF(prior_mu1=1.04,
-                             prior_pi1=np.inf,
-                             omega1=-12.0,
+                             prior_pi1=1e4,
+                             omega1=-13.0,
                              kappa1=1,
                              prior_mu2=1,
-                             prior_pi2=np.inf,
+                             prior_pi2=1e1,
                              omega2=-2,
-                             omega_input=0.0)
+                             omega_input=np.log(1e-4))
 
-    # Give some inputs
-    for u in [0.5, 0.5, 0.5]:
-        stdhgf.input(u)
+    # Read USD-CHF data
+    usdchf = np.loadtxt('usdchf.dat')
 
-    # Has update worked?
-    assert stdhgf.x1.mus[1] == 1.0399909812322021
-    assert stdhgf.x2.mus[1] == 0.99999925013985835
+    # Feed input
+    stdhgf.input(usdchf)
 
-    # Check if NegativePosteriorPrecisionError exception is correctly raised
-    with pytest.raises(hgf.NegativePosteriorPrecisionError):
-        stdhgf.input(1e5)
+    # Load benchmark
+    with open('stdhgf.pickle', 'rb') as f:
+        benchmark = pickle.load(f)
+
+    # Compare to benchmark
+    assert stdhgf.x1.mus == benchmark.x1.mus
+    assert stdhgf.x1.pis == benchmark.x1.pis
+    assert stdhgf.x2.mus == benchmark.x2.mus
+    assert stdhgf.x2.pis == benchmark.x2.pis
