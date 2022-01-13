4a7dc98c0d98ed5220d285e0949c767f275bbf93
LegrandNico <nicolas.legrand@cfin.au.dk>
2022-01-13 14:17:25 +0100

    Add a model class for the JAX HGF.

--- ghgf/model.py
+++ ghgf/model.py
@@ -1,15 +1,12 @@
 # Author: Nicolas Legrand <nicolas.legrand@cfin.au.dk>
 
+from typing import Dict, Optional, Tuple
+
 import jax.numpy as jnp
-from typing import Optional, Tuple, Dict
+from jax.interpreters.xla import DeviceArray
 from jax.lax import scan
-from ghgf.hgf_jax import (
-    update_parents,
-    update_input_parents,
-    gaussian_surprise,
-    loop_inputs,
-    node_validation,
-)
+
+from ghgf.hgf_jax import loop_inputs, node_validation
 
 
 class HGF(object):
@@ -17,32 +14,34 @@ class HGF(object):
 
     def __init__(
         self,
-        n_levels: int = 2,
+        n_levels: Optional[int] = None,
         model_type: Optional[str] = None,
-        initial_mu: Dict[str, float] = {"1": 0.0, "2": 0.0},
-        initial_pi: Dict[str, float] = {"1": 1.0, "2": 1.0},
+        initial_mu: Dict[str, float] = {"1": jnp.array(0.0), "2": jnp.array(0.0)},
+        initial_pi: Dict[str, float] = {"1": jnp.array(1.0), "2": jnp.array(1.0)},
         omega_input: float = jnp.log(1e-4),
         omega: Dict[str, float] = {"1": -10.0, "2": -10.0},
-        kappa: Dict[str, float] = {"1": 1.0},
+        kappa: Dict[str, float] = {"1": jnp.array(1.0)},
         rho: Optional[Dict[str, float]] = None,
         phi: Optional[Dict[str, float]] = None,
         m: Dict[str, float] = None,
         verbose: bool = True,
     ):
 
-        """The standard n-level HGF for continuous inputs.
+        """The standard n-level HGF for continuous inputs with JAX backend.
 
         The standard continuous HGF can implements the Gaussian Random Walk and AR1
         perceptual models.
 
         Parameters
         ----------
-        n_levels : int
-            The number of hierarchies in the perceptual model (default sets to `2`).
+        n_levels : int | None
+            The number of hierarchies in the perceptual model (can be `1` or `2`). If
+            `None`, the nodes hierarchy is not created and might be provided afterward
+            using `add_nodes()`. Default sets to `None`.
         model_type : str or None
-            The model type to use (can be "GRW" or "AR1"). If `model_type` is not provided,
-            it is infered from the parameters provided. If both `phi` and `rho` are None
-            or dictionnary, an error is returned.
+            The model type to use (can be "GRW" or "AR1"). If `model_type` is not
+            provided, it is infered from the parameters provided. If both `phi` and
+            `rho` are None or dictionnary, an error is returned.
         initial_mu : dict
             Dictionnary containing the initial values for the `initial_mu` parameter at
             different levels of the hierarchy. Defaults set to `{"1": 0.0, "2": 0.0}`
@@ -59,20 +58,20 @@ class HGF(object):
             `{"1": -10.0, "2": -10.0}` for a 2-levels model. This parameters only when
             `model_type="GRW"`.
         omega_input : float
-            Default value sets to `np.log(1e-4)`. Represent the noise associated with the
-            input.
+            Default value sets to `np.log(1e-4)`. Represent the noise associated with
+            the input.
         rho : dict | None
             Dictionnary containing the initial values for the `rho` parameter at
-            different levels of the hierarchy. Rho represents the drift of the random walk.
-            Only required when `model_type="GRW"`. Defaults set all entries to `0`
-            according to the number of required levels.
+            different levels of the hierarchy. Rho represents the drift of the random
+            walk. Only required when `model_type="GRW"`. Defaults set all entries to
+            `0` according to the number of required levels.
         kappa : dict
             Dictionnary containing the initial values for the `kappa` parameter at
             different levels of the hierarchy. Kappa represents the phasic part of the
-            variance (the part that is affected by the parents nodes) and will defines the
-            strenght of the connection between the node and the parent node. Often fixed
-            to 1. Defaults set to `{"1": 1.0}` for a 2-levels model. Only required when
-            `model_type="GRW"`.
+            variance (the part that is affected by the parents nodes) and will defines
+            the strenght of the connection between the node and the parent node. Often
+            fixed to 1. Defaults set to `{"1": 1.0}` for a 2-levels model. Only
+            required when `model_type="GRW"`.
         phi : dict | None
             Dictionnary containing the initial values for the `phi` parameter at
             different levels of the hierarchy. Phi should always be between 0 and 1.
@@ -80,31 +79,83 @@ class HGF(object):
             `phi` is only required when `model_type="AR1"`.
         m : dict or None
             Dictionnary containing the initial values for the `m` parameter at
-            different levels of the hierarchy. Defaults set all entries to `0` according to
-            the number of required levels. `m` is only required when `model_type="AR1"`.
+            different levels of the hierarchy. Defaults set all entries to `0`
+            according to the number of required levels. `m` is only required when
+            `model_type="AR1"`.
         verbose : bool
             Default is `True` (show bar progress).
 
         Attributes
         ----------
+        verbose : bool
+            Verbosity level.
         n_levels : int
             The number of hierarchies in the model. Cannot be less than 2.
         model_type : str
             The model implemented (can be `"AR1"` or `"GRW"`).
+        nodes : tuple
+            The nodes hierarchy.
 
         Notes
         -----
         The model used by the perceptual model is defined by the `model_type` parameter
-        (can be `"GRW"` or `"AR1"`). If `model_type` is not provided, the class will try to
-        determine it automatically by looking at the `rho` and `phi` parameters. If `rho`
-        is provided `model_type="GRW"`, if `phi` is provided `model_type="AR1"`. If both
-        `phi` and `rho` are `None` an error will be returned.
+        (can be `"GRW"` or `"AR1"`). If `model_type` is not provided, the class will
+        try to determine it automatically by looking at the `rho` and `phi` parameters.
+        If `rho` is provided `model_type="GRW"`, if `phi` is provided
+        `model_type="AR1"`. If both `phi` and `rho` are `None` an error will be
+        returned.
 
         Examples
         --------
 
         """
 
+        self.verbose = verbose
+        self.n_levels = n_levels
+        if self.verbose:
+            print(
+                (
+                    "Fitting the continuous Hierarchical Gaussian Filter (JAX backend) "
+                    f"with {self.n_levels} levels."
+                )
+            )
+
+        if n_levels == 2:
+
+            # Second level
+            x2_parameters = {
+                "mu": initial_mu["2"],
+                "muhat": jnp.nan,
+                "pi": initial_pi["2"],
+                "pihat": jnp.nan,
+                "kappas": None,
+                "nu": jnp.nan,
+                "psis": None,
+                "omega": omega["2"],
+                "rho": rho["2"],
+            }
+            x2 = x2_parameters, None, None
+
+            # First level
+            x1_parameters = {
+                "mu": initial_mu["1"],
+                "muhat": jnp.nan,
+                "pi": initial_pi["1"],
+                "pihat": jnp.nan,
+                "kappas": (kappa["1"],),
+                "nu": jnp.nan,
+                "psis": None,
+                "omega": omega["1"],
+                "rho": rho["1"],
+            }
+            x1 = x1_parameters, None, (x2,)
+
+            # Input node
+            input_node_parameters = {
+                "kappas": None,
+                "omega": omega_input,
+            }
+            self.input_node = input_node_parameters, x1, None
 
     def add_nodes(self, nodes: Tuple):
         """Add a custom node structure.
@@ -117,17 +168,23 @@ class HGF(object):
 
         """
         node_validation(nodes, input_node=True)
-        self.nodes = nodes
+        self.input_node = nodes
 
-    def input_data(self, input_data):
+    def input_data(
+        self,
+        input_data,
+        time: DeviceArray = jnp.array(0.0),
+        value: DeviceArray = jnp.array(0.0),
+        surprise: DeviceArray = jnp.array(0.0),
+    ):
 
         # Initialise the first values
         res_init = (
-            self.nodes,
+            self.input_node,
             {
-                "time": jnp.array(0.0),
-                "value": jnp.array(0.0),
-                "surprise": jnp.array(0.0),
+                "time": time,
+                "value": value,
+                "surprise": surprise,
             },
         )
 
@@ -137,7 +194,6 @@ class HGF(object):
         # Store ouptut values
         self.last = last  # The last tuple returned
         self.final = final  # The commulative update of the nodes and results
-
 
     def surprise(self):
 
