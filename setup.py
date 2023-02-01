ee5394464296db7806ba5ab5df6cd20edf90dc9a
Nicolas Legrand <nicolas.legrand@cfin.au.dk>
2023-02-01 10:20:43 +0100

    remove non required install for building documentation (#22)
    * remove non-required install for building documentation
    * get package automatically in setup.py
    * rewrite requirement files

--- setup.py
+++ setup.py
@@ -1,8 +1,14 @@
 import codecs
 import os
 
-from setuptools import setup
+from setuptools import setup, find_packages
 
+PROJECT_ROOT = os.path.dirname(os.path.realpath(__file__))
+REQUIREMENTS_FILE = os.path.join(PROJECT_ROOT, "requirements.txt")
+
+def get_requirements():
+    with codecs.open(REQUIREMENTS_FILE) as buff:
+        return buff.read().splitlines()
 
 # Get the package's version number of the __init__.py file
 def read(rel_path):
@@ -31,13 +37,7 @@ DESCRIPTION = "The generalized, nodalized HGF for predictive coding."
 DISTNAME = "ghgf"
 AUTHOR = "ILAB"
 MAINTAINER = "Nicolas Legrand"
-MAINTAINER_EMAIL = "nicolas.legrand@cfin.au.dk"
-INSTALL_REQUIRES = [
-    "jax>=0.3.13",
-    "jaxlib>=0.3.10",
-]
-PACKAGES = ["ghgf"]
-
+MAINTAINER_EMAIL = "nicolas.legrand@cas.au.dk"
 
 
 if __name__ == "__main__":
@@ -53,8 +53,8 @@ if __name__ == "__main__":
         long_description_content_type="text/x-rst",
         license="GPL-3.0",
         version=get_version("ghgf/__init__.py"),
-        install_requires=INSTALL_REQUIRES,
+        install_requires=get_requirements(),
         include_package_data=True,
         package_data={"": ["ghgf/ghgf/data/*.dat"]},
-        packages=PACKAGES,
+        packages=find_packages(),
     )
