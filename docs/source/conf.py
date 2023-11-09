56ba076985541a1e2e89034332d07296af80ecf6
LegrandNico <nicolas.legrand@cas.au.dk>
2023-11-09 08:46:02 +0100

    remove intersphinx

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -41,7 +41,6 @@ extensions = [
     "sphinx.ext.githubpages",
     "sphinx.ext.autosummary",
     "sphinx.ext.autodoc",
-    "sphinx.ext.intersphinx",
     "matplotlib.sphinxext.plot_directive",
     "numpydoc",
     "myst_nb",
@@ -112,16 +111,4 @@ myst_enable_extensions = ["dollarmath", "colon_fence"]
 
 html_sidebars = {
   "api": [],
-}
-
-# -- Intersphinx ------------------------------------------------
-
-intersphinx_mapping = {
-    "numpy": ("https://numpy.org/doc/stable", None),
-    "scipy": ("http://docs.scipy.org/doc/scipy/reference/", None),
-    "matplotlib": ("http://matplotlib.org/", None),
-    "pandas": ("https://pandas.pydata.org/pandas-docs/stable/", None),
-    "seaborn": ("https://seaborn.pydata.org/", None),
-    "python": ("https://docs.python.org/3", None),
-    "jax": ("https://jax.readthedocs.io/en/latest", None),
 }
