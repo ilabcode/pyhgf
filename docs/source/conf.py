aa5e6cedf04df1e9d922dbeef128b988ef901741
LegrandNico <nicolas.legrand@cas.au.dk>
2023-11-17 11:55:18 +0100

    exclude patterns

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -73,7 +73,7 @@ master_doc = "index"
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = []
+exclude_patterns = ["notebooks/*.ipynb"]
 
 # -- Options for HTML output -------------------------------------------------
 
