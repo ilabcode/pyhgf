c7ea7c93d68c667f32c15921e090be533af1ec80
Nicolas Legrand <nicolas.legrand@cfin.au.dk>
2024-06-12 13:57:50 +0200

    v0.1.0

--- src/pyhgf/__init__.py
+++ src/pyhgf/__init__.py
@@ -7,7 +7,7 @@ from typing import Tuple, Union
 import numpy as np
 import pandas as pd
 
-__version__ = "0.0.19"
+__version__ = "0.1.0"
 
 
 def load_data(dataset: str) -> Union[Tuple[np.ndarray, ...], np.ndarray]:
