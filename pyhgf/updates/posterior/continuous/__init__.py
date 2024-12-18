5f690dee6e5502350d511809b4cb7130dc475329
LegrandNico <nicolas.legrand@cas.au.dk>
2024-12-18 11:25:33 +0100

    add tests

--- pyhgf/updates/posterior/continuous/__init__.py
+++ pyhgf/updates/posterior/continuous/__init__.py
@@ -3,6 +3,5 @@ from .continuous_node_posterior_update_ehgf import continuous_node_posterior_upd
 
 __all__ = [
     "continuous_node_posterior_update_ehgf",
-    "continuous_node_posterior_update_unbounded",
     "continuous_node_posterior_update",
 ]
